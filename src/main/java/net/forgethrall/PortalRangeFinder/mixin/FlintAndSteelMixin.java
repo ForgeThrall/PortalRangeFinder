7e6eefb73750d775736ccb555648985ea210180f
ForgeThrall <14297794+forgethrall@users.noreply.github.com>
2022-01-12 21:57:40 -0600

    Flint and steel use hook added

---
+++ src/main/java/net/forgethrall/PortalRangeFinder/mixin/FlintAndSteelMixin.java
@@ -0,0 +1,26 @@
+package net.forgethrall.PortalRangeFinder.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraft.item.FlintAndSteelItem;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FlintAndSteelItem.class)
+public class FlintAndSteelMixin {
+	@Inject(at = @At("HEAD"), method = "useOnBlock", cancellable = true)
+	private void init(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+		World world = context.getWorld();
+		BlockPos blockPos = context.getBlockPos();
+		BlockState blockState = world.getBlockState(blockPos);
+		if(world.isClient() && blockState.getBlock() instanceof NetherPortalBlock) {
+			cir.setReturnValue(ActionResult.SUCCESS);
+		}
+	}
+}
