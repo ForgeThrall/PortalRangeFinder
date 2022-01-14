e7160043230f42cd40491224bde13e6c52765cdd
ForgeThrall <14297794+forgethrall@users.noreply.github.com>
2022-01-14 16:24:26 -0600

    First step to voxel-based approach

---
+++ src/main/java/net/forgethrall/PortalRangeFinder/Mesh.java
@@ -0,0 +1,76 @@
+package net.forgethrall.PortalRangeFinder;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.client.render.*;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
+
+import java.util.HashSet;
+
+public class Mesh {
+	Vec3d[][] tris;
+	Vec3d[][] lines;
+	int red = 230, green = 200, blue = 200, alpha = 90;
+
+	public Mesh(Vec3d[][] tris, Vec3d[][] lines){
+		this.tris = tris;
+		this.lines = lines;
+	}
+
+	public Mesh(HashSet<Vec3d[]> tris, HashSet<Vec3d[]> lines){
+		this(tris.toArray(Vec3d[][]::new), lines.toArray(Vec3d[][]::new));
+	}
+
+	public void render(WorldRenderContext context) {
+		RenderSystem.disableTexture();
+		RenderSystem.polygonOffset(-3f, -3f);
+		RenderSystem.enablePolygonOffset();
+		RenderSystem.disableCull();
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		RenderSystem.depthMask(false);
+
+		Vec3d cam = context.camera().getPos();
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferBuilder = tessellator.getBuffer();
+
+		//todo change this to a triangle_fan or quads?
+		RenderSystem.setShader(GameRenderer::getPositionColorShader);
+		bufferBuilder.begin(VertexFormat.DrawMode.TRIANGLES, VertexFormats.POSITION_COLOR);
+		for(Vec3d[] tri : tris) {
+			Vec3d v0 = cam.relativize(tri[0]);
+			Vec3d v1 = cam.relativize(tri[1]);
+			Vec3d v2 = cam.relativize(tri[2]);
+			bufferBuilder.vertex(v0.x, v0.y, v0.z).color(red, green, blue, 40).next();
+			bufferBuilder.vertex(v1.x, v1.y, v1.z).color(red, green, blue, 40).next();
+			bufferBuilder.vertex(v2.x, v2.y, v2.z).color(red, green, blue, 40).next();
+		}
+		tessellator.draw();
+
+		RenderSystem.setShader(GameRenderer::getRenderTypeLinesShader);
+		RenderSystem.lineWidth(3F);
+		bufferBuilder.begin(VertexFormat.DrawMode.LINES, VertexFormats.LINES);
+		for(Vec3d[] line : lines) {
+			Vec3d v0 = cam.relativize(line[0]);
+			Vec3d v1 = cam.relativize(line[1]);
+			Vec3f lineNormal = new Vec3f(v0.relativize(v1).normalize());
+			bufferBuilder.vertex(v0.x, v0.y, v0.z)
+					.color(red, green, blue, 255)
+					.normal(lineNormal.getX(), lineNormal.getY(), lineNormal.getZ())
+					.next();
+			bufferBuilder.vertex(v1.x, v1.y, v1.z)
+					.color(red, green, blue, 255)
+					.normal(lineNormal.getX(), lineNormal.getY(), lineNormal.getZ())
+					.next();
+		}
+		tessellator.draw();
+
+		RenderSystem.depthMask(true);
+		RenderSystem.enableCull();
+		RenderSystem.polygonOffset(0f, 0f);
+		RenderSystem.disablePolygonOffset();
+		RenderSystem.disableBlend();
+		RenderSystem.enableTexture();
+	}
+}
--- src/main/java/net/forgethrall/PortalRangeFinder/PortalVisualizer.java
+++ src/main/java/net/forgethrall/PortalRangeFinder/PortalVisualizer.java
@@ -6,20 +6,26 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.BlockLocating;
 import net.minecraft.world.World;
 
+import java.util.HashSet;
+
 public class PortalVisualizer {
 	private RegistryKey<World> portalDimension;
-	private Polyhedron framePoly;
-	private Polyhedron rangePoly;
+	Mesh portalMesh;
+	Mesh rangeMesh;
+
+	BlockPos portalOrigin;
+	Vec3i portalSize;
 
 	public PortalVisualizer(World world, BlockPos portalBlockPos) {
 		BlockState blockState = world.getBlockState(portalBlockPos);
 		BlockLocating.Rectangle portalRect = BlockLocating.getLargestRectangle(portalBlockPos, blockState.get(Properties.HORIZONTAL_AXIS), 21, Direction.Axis.Y, 21, pos -> world.getBlockState(pos) == blockState);
 
-		Vec3d portalOrigin = new Vec3d(portalRect.lowerLeft.getX(), portalRect.lowerLeft.getY(), portalRect.lowerLeft.getZ());
+		this.portalOrigin = portalRect.lowerLeft;
 		int xWidth, yWidth;
 		if(blockState.get(Properties.HORIZONTAL_AXIS) == Direction.Axis.X) {
 			xWidth = portalRect.width;
@@ -28,31 +34,74 @@ public class PortalVisualizer {
 			xWidth = 1;
 			yWidth = portalRect.width;
 		}
-		Vec3d portalSize = new Vec3d(xWidth , portalRect.height, yWidth);
+		this.portalSize = new Vec3i(xWidth , portalRect.height, yWidth);
 
 		this.portalDimension = world.getRegistryKey();
-		int rangeRadius, rangeBottom, rangeTop;
-		if(portalDimension == World.OVERWORLD){
-			rangeRadius = 16;
-			rangeBottom = 0;
-			rangeTop = 256;
-		} else {
-			rangeRadius = 128;
-			rangeBottom = -64;
-			rangeTop = 320;
-		}
+//		int rangeRadius, rangeBottom, rangeTop;
+//		if(portalDimension == World.OVERWORLD){
+//			rangeRadius = 16;
+//			rangeBottom = 0;
+//			rangeTop = 256;
+//		} else {
+//			rangeRadius = 128;
+//			rangeBottom = -64;
+//			rangeTop = 320;
+//		}
 
-		this.framePoly = Polyhedron.rectangularCuboid(portalOrigin, portalSize);
-		Vec3d rangeOrigin = new Vec3d(portalOrigin.x - rangeRadius,rangeBottom, portalOrigin.z - rangeRadius);
-		Vec3d rangeSize = new Vec3d(portalSize.x + 2 * rangeRadius, rangeTop, portalSize.z + 2* rangeRadius);
-		this.rangePoly = Polyhedron.rectangularCuboid(rangeOrigin, rangeSize);
+		generateFrameMesh();
+//		generateRangeMesh();
 	}
 
 	public void render(WorldRenderContext context) {
 		if(context.world().getRegistryKey() == portalDimension) {
-			framePoly.render(context);
+			portalMesh.render(context);
 		} else {
-			rangePoly.render(context);
+			rangeMesh.render(context);
 		}
+	}
+
+	private void generateFrameMesh() {
+		HashSet<Vec3d[]> tris = new HashSet<>();
+		HashSet<Vec3d[]> lines = new HashSet<>();
+
+		Vec3d v000 = new Vec3d(portalOrigin.getX(), portalOrigin.getY(), portalOrigin.getZ());
+		Vec3d v100 = new Vec3d(portalOrigin.getX() + portalSize.getX(), portalOrigin.getY(), portalOrigin.getZ());
+		Vec3d v001 = new Vec3d(portalOrigin.getX(), portalOrigin.getY(), portalOrigin.getZ() + portalSize.getZ());
+		Vec3d v101 = new Vec3d(portalOrigin.getX() + portalSize.getX(), portalOrigin.getY(), portalOrigin.getZ() + portalSize.getZ());
+		Vec3d v010 = new Vec3d(portalOrigin.getX(), portalOrigin.getY() + portalSize.getY(), portalOrigin.getZ());
+		Vec3d v110 = new Vec3d(portalOrigin.getX() + portalSize.getX(), portalOrigin.getY() + portalSize.getY(), portalOrigin.getZ());
+		Vec3d v011 = new Vec3d(portalOrigin.getX(), portalOrigin.getY() + portalSize.getY(), portalOrigin.getZ() + portalSize.getZ());
+		Vec3d v111 = new Vec3d(portalOrigin.getX() + portalSize.getX(), portalOrigin.getY() + portalSize.getY(), portalOrigin.getZ() + portalSize.getZ());
+
+		tris.add(new Vec3d[]{v000, v100, v101});
+		tris.add(new Vec3d[]{v000, v101, v001});
+		tris.add(new Vec3d[]{v000, v001, v011});
+		tris.add(new Vec3d[]{v000, v011, v010});
+		tris.add(new Vec3d[]{v000, v010, v110});
+		tris.add(new Vec3d[]{v000, v110, v100});
+
+		tris.add(new Vec3d[]{v111, v110, v010});
+		tris.add(new Vec3d[]{v111, v010, v011});
+		tris.add(new Vec3d[]{v111, v011, v001});
+		tris.add(new Vec3d[]{v111, v001, v101});
+		tris.add(new Vec3d[]{v111, v101, v100});
+		tris.add(new Vec3d[]{v111, v100, v110});
+
+		lines.add(new Vec3d[]{v000, v100});
+		lines.add(new Vec3d[]{v100, v101});
+		lines.add(new Vec3d[]{v101, v001});
+		lines.add(new Vec3d[]{v001, v000});
+
+		lines.add(new Vec3d[]{v111, v011});
+		lines.add(new Vec3d[]{v011, v010});
+		lines.add(new Vec3d[]{v010, v110});
+		lines.add(new Vec3d[]{v110, v111});
+
+		lines.add(new Vec3d[]{v000, v010});
+		lines.add(new Vec3d[]{v001, v011});
+		lines.add(new Vec3d[]{v101, v111});
+		lines.add(new Vec3d[]{v100, v110});
+
+		this.portalMesh = new Mesh(tris, lines);
 	}
 }
