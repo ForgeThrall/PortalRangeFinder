732c3d4d2c8f139717850e6fe376b3527cc35bc1
ForgeThrall <14297794+forgethrall@users.noreply.github.com>
2022-01-15 14:57:23 -0600

    Consolidate mesh generation

--- src/main/java/net/forgethrall/PortalRangeFinder/PortalVisualizer.java
+++ src/main/java/net/forgethrall/PortalRangeFinder/PortalVisualizer.java
@@ -48,11 +48,11 @@ public class PortalVisualizer {
 //			rangeTop = 320;
 //		}
 
-		// nether range
-//		voxels = new boolean[256+portalSize.getX()][256][256+portalSize.getY()];
-
-		// overworld range
-		voxels = new boolean[32+portalSize.getX()][384][32+portalSize.getZ()];
+		if(portalDimension == World.OVERWORLD) {
+			voxels = new boolean[256+portalSize.getX()][256][256+portalSize.getY()];
+		} else {
+			voxels = new boolean[32+portalSize.getX()][384][32+portalSize.getZ()];
+		}
 
 		for(boolean[][] a : voxels) {
 			for(boolean[] b : a) {
@@ -113,75 +113,35 @@ public class PortalVisualizer {
 	}
 
 	private void generateRangeMesh() {
-		generateRangeMeshOverworld();
-	}
-
-	private void generateRangeMeshOverworld() {
 		HashSet<Vec3d[]> quads = new HashSet<>();
 		HashSet<Vec3d[]> lines = new HashSet<>();
 
-		int x0 = portalOrigin.getX()*8-128;
-		int y0 = -64;
-		int z0 = portalOrigin.getZ()*8-128;
-		for(int x = 0; x < voxels.length; x++) {
-			for(int y = 0; y < voxels[0].length; y++) {
-				for(int z = 0; z < voxels[0][0].length; z++) {
-					if(!voxels[x][y][z]) continue;
-
-					Vec3d v000 = new Vec3d(x0+x*8,   y0+y,   z0+z*8);
-					Vec3d v100 = new Vec3d(x0+x*8+8, y0+y,   z0+z*8);
-					Vec3d v001 = new Vec3d(x0+x*8,   y0+y,   z0+z*8+8);
-					Vec3d v101 = new Vec3d(x0+x*8+8, y0+y,   z0+z*8+8);
-					Vec3d v010 = new Vec3d(x0+x*8,   y0+y+1, z0+z*8);
-					Vec3d v110 = new Vec3d(x0+x*8+8, y0+y+1, z0+z*8);
-					Vec3d v011 = new Vec3d(x0+x*8,   y0+y+1, z0+z*8+8);
-					Vec3d v111 = new Vec3d(x0+x*8+8, y0+y+1, z0+z*8+8);
-
-					if(x+1 >= voxels.length || !voxels[x+1][y][z]){
-						quads.add(new Vec3d[]{v100, v110, v111, v101});
-					}
-					if(x-1 < 0 || !voxels[x-1][y][z]) {
-						quads.add(new Vec3d[]{v000, v001, v011, v010});
-					}
-					if(y+1 >= voxels[0].length || !voxels[x][y+1][z]) {
-						quads.add(new Vec3d[]{v010, v011, v111, v110});
-					}
-					if(y-1 < 0 || !voxels[x][y-1][z]) {
-						quads.add(new Vec3d[]{v000, v100, v101, v001});
-					}
-					if(z+1 >= voxels[0][0].length || !voxels[x][y][z+1]) {
-						quads.add(new Vec3d[]{v001, v101, v111, v011});
-					}
-					if(z-1 < 0 || !voxels[x][y][z-1]) {
-						quads.add(new Vec3d[]{v000, v010, v110, v100});
-					}
-				}
-			}
+		final double scale, x0, y0, z0;
+		if(portalDimension == World.OVERWORLD) {
+			scale = 1.0/8;
+			x0 = portalOrigin.getX()*scale - 16;
+			y0 = 0;
+			z0 = portalOrigin.getZ()*scale - 16;
+		} else {
+			scale = 8;
+			x0 = portalOrigin.getX()*scale-128;
+			y0 = -64;
+			z0 = portalOrigin.getZ()*scale-128;
 		}
 
-		rangeMesh = new Mesh(quads, lines);
-	}
-
-	private void generateRangeMeshNether() {
-		HashSet<Vec3d[]> quads = new HashSet<>();
-		HashSet<Vec3d[]> lines = new HashSet<>();
-
-		double x0 = portalOrigin.getX()/8.0d - 16;
-		double y0 = 0;
-		double z0 = portalOrigin.getZ()/8.0d - 16;
 		for(int x = 0; x < voxels.length; x++) {
 			for(int y = 0; y < voxels[0].length; y++) {
 				for(int z = 0; z < voxels[0][0].length; z++) {
 					if(!voxels[x][y][z]) continue;
 
-					Vec3d v000 = new Vec3d(x0+x/8.0d,      y0+y,   z0+z/8.0d);
-					Vec3d v100 = new Vec3d(x0+x/8.0d+.125, y0+y,   z0+z/8.0d);
-					Vec3d v001 = new Vec3d(x0+x/8.0d,      y0+y,   z0+z/8.0d+.125);
-					Vec3d v101 = new Vec3d(x0+x/8.0d+.125, y0+y,   z0+z/8.0d+.125);
-					Vec3d v010 = new Vec3d(x0+x/8.0d,      y0+y+1, z0+z/8.0d);
-					Vec3d v110 = new Vec3d(x0+x/8.0d+.125, y0+y+1, z0+z/8.0d);
-					Vec3d v011 = new Vec3d(x0+x/8.0d,      y0+y+1, z0+z/8.0d+.125);
-					Vec3d v111 = new Vec3d(x0+x/8.0d+.125, y0+y+1, z0+z/8.0d+.125);
+					Vec3d v000 = new Vec3d(x0+x*scale,     y0+y,   z0+z*scale);
+					Vec3d v100 = new Vec3d(x0+(x+1)*scale, y0+y,   z0+z*scale);
+					Vec3d v001 = new Vec3d(x0+x*scale,     y0+y,   z0+(z+1)*scale);
+					Vec3d v101 = new Vec3d(x0+(x+1)*scale, y0+y,   z0+(z+1)*scale);
+					Vec3d v010 = new Vec3d(x0+x*scale,     y0+y+1, z0+z*scale);
+					Vec3d v110 = new Vec3d(x0+(x+1)*scale, y0+y+1, z0+z*scale);
+					Vec3d v011 = new Vec3d(x0+x*scale,     y0+y+1, z0+(z+1)*scale);
+					Vec3d v111 = new Vec3d(x0+(x+1)*scale, y0+y+1, z0+(z+1)*scale);
 
 					if(x+1 >= voxels.length || !voxels[x+1][y][z]){
 						quads.add(new Vec3d[]{v100, v110, v111, v101});
